bcbc1b4ba070e54d95cea1e45bfc3608d076c947
akhil272 <akhilms272@gmail.com>
2021-09-30 19:07:08 +0530

    boilerplate next js with tailwindcss

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+function Homepage() {
+  return (
+    <>
+      <h1 className="text-center my-24 font-black tracking-tight text-6xl">
+        Title grid with tailwind css
+      </h1>
+    </>
+  );
+}
+
+export default Homepage;
